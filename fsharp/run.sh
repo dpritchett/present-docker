f3c03d0abd1de6a1a2c6dd53bd51adede9b9e72f
Daniel J. Pritchett <dpritchett@gmail.com>
2014-07-01 13:53:18 -0500

    Now running an example in each folder

--- fsharp/run.sh
+++ fsharp/run.sh
@@ -4,6 +4,6 @@
 
 docker build --quiet="true" --tag=$TAG .
 
-echo "\n** Running ${RUNNER} in container..."
+echo "\n** Running ${RUNNER} in container...\n"
 docker run   --rm -t -i $TAG \
   $RUNNER $@
