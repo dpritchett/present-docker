7d23da372b3d883ed134ef7a3e4c9a6189e2f63c
Daniel J. Pritchett <dpritchett@gmail.com>
2014-07-14 11:20:20 -0500

    Added elixir runner

--- elixir/examples/example.exs
+++ elixir/examples/example.exs
@@ -1,7 +1,8 @@
-# START OMIT
+#!/usr/local/bin/elixir-run
+
 fruit = HashDict.new
 |> HashDict.put(:apple, 1)
 |> HashDict.put(:banana, 3)
 |> HashDict.put(:cherry, 10)
-# END OMIT
+
 IO.inspect fruit
